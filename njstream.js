f353c8e297fe8e667055485d6be7d24fc0436d68
Dan Zajdband <dan.zajdband@gmail.com>
2012-10-23 15:45:27 -0300

    handle bad tokens

--- njstream.js
+++ njstream.js
@@ -14,13 +14,18 @@ util.inherits(NJStream, Stream);
 
 NJStream.prototype.parse = function(data) {
   var self = this;
+
+  if("string" != typeof data) {
+    data = data.toString();
+  }
+
   var delim = data.indexOf(self.delimiter);
   if(delim > 0) {
     self.buffer += data.substr(0, delim);
     try {
       self.emit('parsed', JSON.parse(self.buffer));
     } catch(e) {
-      self.emit('error', e);
+      self.emit('error', new Error('Bad token: ' + data));
     }
 
     self.buffer = '';
